c01d8bc9692bfe58b57b6955bb4d8ae5029db0a3
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-10-21 22:56:28 +0600

    Done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) =>{
     res.send('News API Running');
@@ -14,5 +15,27 @@ app.get('/', (req, res) =>{
 app.get('/news-categories', (req,res)=>{
     res.send(categories)
 })
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/news', (req,res)=>{
+    res.send(news);
+})
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }
+    else{
+        const category_News = news.filter(c=>c.category_id === id);
+        res.send(category_News)
+    }
+})
+
 
 app.listen(port,()=>console.log('News-365 Surver is running in port',port))
